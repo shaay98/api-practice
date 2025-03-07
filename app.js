ea244b19b334cb0d8b618107cc95c66f5723551b
shaay98 <nrichardson567@icloud.com>
2025-03-07 03:41:15 +0000

    working in JS file rendering API

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const url = "https://pokeapi.co/api/v2/pokemon/";
+
+async function getData() {
+    const response = await fetch (url);
+    console.log(response)
+    
+let renderList = pokemon.name
+    console.log(pokemon.name)
+} 
+getData();
+
+
